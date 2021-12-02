6c11b2c949b553c978130365a144588ac91a342d
Robert Alexander Limas Sierra <robalex123@outlook.es>
2021-12-01 20:01:54 -0500

    feature: add buttons in form sign

--- app/src/main/java/com/example/ifound/MainActivity.java
+++ app/src/main/java/com/example/ifound/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         Button btnLogin = (Button) findViewById(R.id.btnLogin);
         btnLogin.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/example/ifound/form_sign.java
+++ app/src/main/java/com/example/ifound/form_sign.java
@@ -1,5 +1,6 @@
 package com.example.ifound;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
